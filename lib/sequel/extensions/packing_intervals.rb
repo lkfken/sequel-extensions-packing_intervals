6f7662f1037d6cfdef9be354e1d9fced38f85dc4
Kenneth Leung <kenneth@leungs.us>
2019-06-14 11:04:37 -0700

    minor update

--- lib/sequel/extensions/packing_intervals.rb
+++ lib/sequel/extensions/packing_intervals.rb
@@ -8,6 +8,8 @@ module Sequel
         raise PackingIntervals::Error, "#{missing_columns} #{missing_columns.size == 1 ? 'is' : 'are'} not found in the dataset!" unless (missing_columns).empty?
 
         grpnm_partition = partition | [:grpnm]
+        lag_end_date = Sequel.function(:lag, :end_date).over(:partition => partition, :order => [:start_date, :end_date])
+
         db = dataset.db
         reduced = db[:c4].
             with(:c1, dataset.cross_apply(DST::DB['VALUES (1, START_DATE), (-1, END_DATE)'].as(:a, [:type, :ts])).
@@ -17,17 +19,16 @@ module Sequel
                     Sequel.case({1 => nil}, Sequel.function(:row_number).over(:partition => [partition, :type].flatten, :order => :end_date), :type).as(:e))).
             with(:c2, db[:c1].select(*partition, :ts, :type, :s, :e).select_append {row_number.function.over(:partition => partition, :order => [:ts, type.desc]).as(:se)}).
             with(:c3, db[:c2].select_append(grpnm(partition: partition).as(:grpnm)).where(Sequel.lit('COALESCE(s - (se - s) - 1, (se - e) - e) = 0'))).
-            with(:c4, db[:c3].select_group(*grpnm_partition).select_append {min(:ts).as(:start_date)}.select_append {max(:ts).as(:end_date)}).
-            select(*partition, Sequel[:c4][:start_date], Sequel[:c4][:end_date])
+            with(:c4, db[:c3].select_group(*grpnm_partition).select_append {min(:ts).as(:start_date)}.select_append {max(:ts).as(:end_date)})
 
-        lag_end_date = Sequel.function(:lag, :end_date).over(:partition => partition, :order => [:start_date, :end_date])
-        db[:p3].
-            with(:p1, reduced.select_append(lag_end_date.as(:lag), Sequel.case({Sequel[:start_date] <= Sequel.expr(lag_end_date + 1) => 0}, 1).as(:grp_start))).
-            with(:p2, db[:p1].select_append(Sequel.function(:sum, :grp_start).over(partition: partition, order: [:start_date, :end_date]).as(:grp))).
-            with(:p3, db[:p2].select_group(*(partition | [:grp])).select_append(
+        # merge date intervals if no gap
+        db[:c7].
+            with(:c5, reduced.select_append(lag_end_date.as(:lag), Sequel.case({Sequel[:start_date] <= Sequel.expr(lag_end_date + 1) => 0}, 1).as(:grp_start))).
+            with(:c6, db[:c5].select_append(Sequel.function(:sum, :grp_start).over(partition: partition, order: [:start_date, :end_date]).as(:grp))).
+            with(:c7, db[:c6].select_group(*(partition | [:grp])).select_append(
                 Sequel.function(:min, :start_date).as(:start_date),
                 Sequel.function(:max, :end_date).as(:end_date))).
-            select(*partition, Sequel[:p3][:start_date], Sequel[:p3][:end_date])
+            select(*partition, Sequel[:c7][:start_date], Sequel[:c7][:end_date])
       end
 
       private
@@ -40,7 +41,7 @@ module Sequel
 
       class Error < StandardError;
       end
-      # Your code goes here...
+
     end
   end
   Sequel::Dataset.register_extension(:packing_intervals, Sequel::Extensions::PackingIntervals)
