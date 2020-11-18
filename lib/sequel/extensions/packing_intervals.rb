b59916977b5a648eac133dc91b8e2e81695ba5cb
Kenneth Leung <kenneth@leungs.us>
2020-11-17 21:39:31 -0800

    allow end user to define "order" in case of a partition have more than one duplicate "start date" or duplicate "end date"

--- lib/sequel/extensions/packing_intervals.rb
+++ lib/sequel/extensions/packing_intervals.rb
@@ -3,7 +3,8 @@ require_relative 'packing_intervals/version'
 module Sequel
   module Extensions
     module PackingIntervals
-      def packing_intervals(partition: nil, dataset: self, start_date: :start_date, end_date: :end_date, cte_alias: :cte, order: [:start_date, :end_date])
+      def packing_intervals(partition: nil, dataset: self, start_date: :start_date, end_date: :end_date, cte_alias: :cte, order: nil)
+        order = order.nil? ? [:start_date, :end_date] : [start_date, end_date]
         unless dataset.where(Sequel.lit('? > ?', start_date, end_date)).limit(1).all.empty?
           raise PackingIntervals, 'ERROR: dataset contain at least one record with start date after end date'
         end
