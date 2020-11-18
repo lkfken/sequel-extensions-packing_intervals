e11ccc247e8becc6a0ebfb8d787e41da47f3c330
Kenneth Leung <kenneth@leungs.us>
2020-11-17 21:38:03 -0800

    allow end user to define "order" in case of a partition have more than one duplicate "start date" or duplicate "end date"

--- lib/sequel/extensions/packing_intervals.rb
+++ lib/sequel/extensions/packing_intervals.rb
@@ -3,7 +3,7 @@ require_relative 'packing_intervals/version'
 module Sequel
   module Extensions
     module PackingIntervals
-      def packing_intervals(partition: nil, dataset: self, start_date: :start_date, end_date: :end_date, cte_alias: :cte)
+      def packing_intervals(partition: nil, dataset: self, start_date: :start_date, end_date: :end_date, cte_alias: :cte, order: [:start_date, :end_date])
         unless dataset.where(Sequel.lit('? > ?', start_date, end_date)).limit(1).all.empty?
           raise PackingIntervals, 'ERROR: dataset contain at least one record with start date after end date'
         end
@@ -19,14 +19,14 @@ module Sequel
         raise PackingIntervals::Error, "#{missing_columns} #{missing_columns.size == 1 ? 'is' : 'are'} not found in the dataset!" unless (missing_columns).empty?
 
         grpnm_partition = partition | [:grpnm]
-        lag_end_date    = Sequel.function(:lag, end_date).over(:partition => partition, :order => [start_date, end_date])
+        lag_end_date    = Sequel.function(:lag, end_date).over(:partition => partition, :order => order)
 
         reduced = db["#{cte_alias}4".to_sym].
             with("#{cte_alias}1".to_sym, dataset.cross_apply(db["VALUES (1, #{start_date.to_s}), (-1, #{end_date.to_s})"].as(:a, [:type, :ts])).
                 select(*partition, :ts, :type).
                 select_append(
-                    Sequel.case({-1 => nil}, Sequel.function(:row_number).over(:partition => [partition, :type].flatten, :order => start_date), :type).as(:s),
-                    Sequel.case({1 => nil}, Sequel.function(:row_number).over(:partition => [partition, :type].flatten, :order => end_date), :type).as(:e))).
+                    Sequel.case({-1 => nil}, Sequel.function(:row_number).over(:partition => [partition, :type].flatten, :order => order), :type).as(:s),
+                    Sequel.case({1 => nil}, Sequel.function(:row_number).over(:partition => [partition, :type].flatten, :order => order), :type).as(:e))).
             with("#{cte_alias}2".to_sym, db["#{cte_alias}1".to_sym].select(*partition, :ts, :type, :s, :e).select_append { row_number.function.over(:partition => partition, :order => [:ts, type.desc]).as(:se) }).
             with("#{cte_alias}3".to_sym, db["#{cte_alias}2".to_sym].select_append(grpnm(partition: partition).as(:grpnm)).where(Sequel.lit('COALESCE(s - (se - s) - 1, (se - e) - e) = 0'))).
             with("#{cte_alias}4".to_sym, db["#{cte_alias}3".to_sym].select_group(*grpnm_partition).select_append { min(:ts).as(start_date) }.select_append { max(:ts).as(end_date) })
@@ -34,7 +34,7 @@ module Sequel
         # merge date intervals if no gap
         db["#{cte_alias}7".to_sym].
             with("#{cte_alias}5".to_sym, reduced.select_append(lag_end_date.as(:lag), Sequel.case({Sequel[start_date] <= Sequel.function(:dateadd, :day, 1, lag_end_date) => 0}, 1).as(:grp_start))).
-            with("#{cte_alias}6".to_sym, db["#{cte_alias}5".to_sym].select_append(Sequel.function(:sum, :grp_start).over(partition: partition, order: [start_date, end_date]).as(:grp))).
+            with("#{cte_alias}6".to_sym, db["#{cte_alias}5".to_sym].select_append(Sequel.function(:sum, :grp_start).over(partition: partition, order: order).as(:grp))).
             with("#{cte_alias}7".to_sym, db["#{cte_alias}6".to_sym].select_group(*(partition | [:grp])).select_append(
                 Sequel.function(:min, start_date).as(start_date),
                 Sequel.function(:max, end_date).as(end_date))).
